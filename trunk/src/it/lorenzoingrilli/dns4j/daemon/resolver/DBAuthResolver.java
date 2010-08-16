80257ee3c85f871284227af60fd533b730793f0b
amlet0 <amlet0@9dccab56-d7e6-4a77-bd1a-42343525f512>
2010-08-16 11:43:27 +0000

    added database resolver

--- trunk/src/it/lorenzoingrilli/dns4j/daemon/resolver/DBAuthResolver.java
+++ trunk/src/it/lorenzoingrilli/dns4j/daemon/resolver/DBAuthResolver.java
@@ -2,7 +2,13 @@ package it.lorenzoingrilli.dns4j.daemon.resolver;
 
 import it.lorenzoingrilli.dns4j.protocol.Type;
 import it.lorenzoingrilli.dns4j.protocol.rr.impl.AImpl;
+import it.lorenzoingrilli.dns4j.protocol.rr.impl.CNameImpl;
+import it.lorenzoingrilli.dns4j.protocol.rr.impl.MxImpl;
+import it.lorenzoingrilli.dns4j.protocol.rr.impl.NsImpl;
+import it.lorenzoingrilli.dns4j.protocol.rr.impl.PtrImpl;
+import it.lorenzoingrilli.dns4j.protocol.rr.impl.SoaImpl;
 
+import java.beans.ConstructorProperties;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.sql.Connection;
@@ -23,28 +29,103 @@ import javax.sql.DataSource;
 public class DBAuthResolver extends AuthoritativeResolver {
 			
 	private DataSource dataSource;
-	private String a = "SELECT id,ttl,address FROM a WHERE name=?";	
-		
+
+	private String sqlQuerySoa		= "SELECT ttl, serial, refresh, expire, retry, minimum, email FROM soa WHERE name=?";
+	private String sqlQueryA		= "SELECT ttl, address FROM a WHERE name=?";
+	private String sqlQueryNs		= "SELECT ttl, nsdname AS host FROM ns WHERE name=?";	
+	private String sqlQueryCname	= "SELECT ttl, cname AS host FROM cname WHERE name=?";	
+	private String sqlQueryPtr 		= "SELECT ttl, ptrdname AS host FROM ptr WHERE name=?";
+	private String sqlQueryMx 		= "SELECT ttl, preference AS priority, exchange AS host FROM mx WHERE name=?";
+	
+	//private String sqlQueryAaaa = null;
+	//private String sqlQueryTxt = null;
+	//private String sqlQuerySrv = null;
+	//private String sqlQueryHinfo = null;
+	//private String sqlQueryRr = null;
+				
+	@ConstructorProperties(value={"dataSource"})
+	public DBAuthResolver(DataSource ds) {
+		setDataSource(ds);
+	}
+	
 	@Override
 	public QuestionResponse query(String qname, int qclass, int qtype) {
 		try {
 		QuestionResponse qr = null;
 		List<Map<String, Object>> list = null;
 		switch (qtype) {
-		case Type.A:
-			list = sqlQuery(a, qname);
+		case Type.SOA:
+			list = sqlQuery(sqlQuerySoa, qname);
 			if(list.size()>0) {
 				qr = new QuestionResponse();
 				for(Map<String, Object> r: list) {
-					AImpl rr = new AImpl();
-					rr.setTtl((Integer) r.get("ttl"));
-					rr.setAddress((Inet4Address) InetAddress.getByName((String) r.get("address")));
-					rr.setName(qname);
+					SoaImpl rr = 
+						new SoaImpl(
+								qname,
+								(Long) r.get("ttl"),
+								qname,
+								((String) r.get("email")).replace('@', '.'), 
+								(Long) r.get("serial"), 
+								(Long) r.get("refresh"),
+								(Long) r.get("retry"),
+								(Long) r.get("expire"),
+								(Long) r.get("minimum")
+								);					
+					qr.getAnswer().add(rr);
+				}
+			}
+			break;			
+		case Type.A:
+			list = sqlQuery(sqlQueryA, qname);
+			if(list.size()>0) {
+				qr = new QuestionResponse();
+				for(Map<String, Object> r: list) {					
+					AImpl rr = new AImpl(qname, (Inet4Address) InetAddress.getByName((String) r.get("address")), (Long) r.get("ttl"));
 					qr.getAnswer().add(rr);
 				}
 			}
 			break;
-
+		case Type.NS:
+			list = sqlQuery(sqlQueryNs, qname);
+			if(list.size()>0) {
+				qr = new QuestionResponse();
+				for(Map<String, Object> r: list) {
+					NsImpl rr = new NsImpl(qname, (String) r.get("host"), (Long) r.get("ttl"));
+					qr.getAnswer().add(rr);
+				}
+			}
+			break;
+		case Type.CNAME:
+			list = sqlQuery(sqlQueryCname, qname);
+			if(list.size()>0) {
+				qr = new QuestionResponse();
+				for(Map<String, Object> r: list) {
+					CNameImpl rr = new CNameImpl(qname, (String) r.get("host"), (Long) r.get("ttl"));
+					qr.getAnswer().add(rr);
+				}
+			}
+			break;
+		case Type.PTR:
+			InetAddress addr = PtrImpl.nameToAddress(qname);
+			list = sqlQuery(sqlQueryPtr, addr.getHostAddress());
+			if(list.size()>0) {
+				qr = new QuestionResponse();
+				for(Map<String, Object> r: list) {
+					PtrImpl rr = new PtrImpl(qname, (String) r.get("host"), (Long) r.get("ttl"));
+					qr.getAnswer().add(rr);
+				}
+			}
+			break;
+		case Type.MX:
+			list = sqlQuery(sqlQueryMx, qname);
+			if(list.size()>0) {
+				qr = new QuestionResponse();
+				for(Map<String, Object> r: list) {
+					MxImpl rr = new MxImpl(qname, (Long) r.get("ttl"), (String) r.get("host"), (int)(long)(Long) r.get("priority"));
+					qr.getAnswer().add(rr);
+				}
+			}
+			break;
 		default:
 			break;
 		}
@@ -55,7 +136,7 @@ public class DBAuthResolver extends AuthoritativeResolver {
 			return null;
 		}
 	}
-	
+		
 	protected List<Map<String, Object>> sqlQuery(String sql, Object ...params) throws SQLException {
 		List<Map<String, Object>> list = new LinkedList<Map<String, Object>>();
 		Connection connection = null;
@@ -98,6 +179,54 @@ public class DBAuthResolver extends AuthoritativeResolver {
 
 	public void setDataSource(DataSource dataSource) {
 		this.dataSource = dataSource;
+	}
+
+	public String getSqlQuerySoa() {
+		return sqlQuerySoa;
+	}
+
+	public void setSqlQuerySoa(String sqlQuerySoa) {
+		this.sqlQuerySoa = sqlQuerySoa;
+	}
+
+	public String getSqlQueryA() {
+		return sqlQueryA;
+	}
+
+	public void setSqlQueryA(String sqlQueryA) {
+		this.sqlQueryA = sqlQueryA;
+	}
+
+	public String getSqlQueryCname() {
+		return sqlQueryCname;
+	}
+
+	public void setSqlQueryCname(String sqlQueryCname) {
+		this.sqlQueryCname = sqlQueryCname;
+	}
+
+	public String getSqlQueryPtr() {
+		return sqlQueryPtr;
+	}
+
+	public void setSqlQueryPtr(String sqlQueryPtr) {
+		this.sqlQueryPtr = sqlQueryPtr;
+	}
+
+	public String getSqlQueryNs() {
+		return sqlQueryNs;
+	}
+
+	public void setSqlQueryNs(String sqlQueryNs) {
+		this.sqlQueryNs = sqlQueryNs;
+	}
+
+	public String getSqlQueryMx() {
+		return sqlQueryMx;
+	}
+
+	public void setSqlQueryMx(String sqlQueryMx) {
+		this.sqlQueryMx = sqlQueryMx;
 	}
 	
 }
