639569859bbdba008ba08a032d28fd666d6a77bd
amlet0 <amlet0@9dccab56-d7e6-4a77-bd1a-42343525f512>
2010-10-10 08:51:39 +0000

    added copyright information

--- src-core/it/lorenzoingrilli/dns4j/resolver/NetEventListener.java
+++ src-core/it/lorenzoingrilli/dns4j/resolver/NetEventListener.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) 2010 - Lorenzo Ingrilli' - info@lorenzoingrilli.it
+ * 
+ * Licensed under LGPL3 only.
+ * See http://www.gnu.org/licenses/lgpl.html
+ */
 package it.lorenzoingrilli.dns4j.resolver;
 
 /**
