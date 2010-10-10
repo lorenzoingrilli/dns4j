5a8b121478679461bff68e4c6df1e600392cd3bd
amlet0 <amlet0@9dccab56-d7e6-4a77-bd1a-42343525f512>
2010-10-10 08:51:39 +0000

    added copyright information

--- trunk/src-core/it/lorenzoingrilli/dns4j/resolver/DnsEventListener.java
+++ trunk/src-core/it/lorenzoingrilli/dns4j/resolver/DnsEventListener.java
@@ -1,9 +1,15 @@
+/*
+ * Copyright (C) 2010 - Lorenzo Ingrilli' - info@lorenzoingrilli.it
+ * 
+ * Licensed under LGPL3 only.
+ * See http://www.gnu.org/licenses/lgpl.html
+ */
 package it.lorenzoingrilli.dns4j.resolver;
 
 import it.lorenzoingrilli.dns4j.protocol.Message;
 
 /**
- * @author Lorenzo Ingrilli' <info@lorenzoingrilli.it>
+ * @author Lorenzo Ingrilli'
  */
 public interface DnsEventListener {
     public void onException(byte[] message, Exception e);
